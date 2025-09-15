ce426e99ed279fd26607e795d22be27b0ebe49f7
Bangsur <bangsuryapratama@gmail.com>
2025-09-15 17:08:43 +0700

    fix

--- app/Filament/Resources/CategoryBlogs/Schemas/CategoryBlogForm.php
+++ app/Filament/Resources/CategoryBlogs/Schemas/CategoryBlogForm.php
@@ -11,17 +11,20 @@ class CategoryBlogForm
     public static function configure(Schema $schema): Schema
     {
         return $schema->schema([
+            // Nama Kategori
             Forms\Components\TextInput::make('name')
                 ->label('Nama Kategori')
                 ->required()
                 ->maxLength(255)
                 ->live(onBlur: true)
-                ->afterStateUpdated(fn ($state, callable $set) => 
-                    $set('slug', Str::slug($state))
-                ),
+                ->afterStateUpdated(function ($state, callable $set) {
+                    $set('slug', Str::slug($state));
+                }),
 
+            // Slug (otomatis, tidak bisa diubah manual)
             Forms\Components\TextInput::make('slug')
                 ->label('Slug')
+                ->disabled() // tidak bisa diinput
                 ->required()
                 ->unique(ignoreRecord: true)
                 ->maxLength(255),

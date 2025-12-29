a462ef4f8bfb52f3134880331c482cc7959624a5
Cecep Saeful Azhar Hidayat, ST <122300501+cecep-azhar@users.noreply.github.com>
2025-12-29 22:11:48 +0700

    add secure link

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,9 +16,19 @@ return Application::configure(basePath: dirname(__DIR__))
         // Tambahkan middleware ke web group
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
-            
-            
+
+
         ]);
+        // Tambahkan block ini:
+        $middleware->trustProxies(at: '*');
+
+        // Opsional: Untuk keamanan ekstra agar redirect HTTPS otomatis
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

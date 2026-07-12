becf4e644166619e052904ab462d694a9d765163
Cecep Azhar <cecep@fathforce.com>
2026-07-12 17:21:25 +0700

    fix: Remove route:cache and config:cache - breaks Filament dynamic routes

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 php artisan cache:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+php artisan view:clear
 php artisan queue:work --sleep=3 --tries=3 --max-time=3600 &
 php artisan serve --host=0.0.0.0 --port=8000

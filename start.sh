8e6bd2542b96412bc52210e6dfc68538d95533e3
Cecep Azhar <cecep@fathforce.com>
2026-07-12 17:18:23 +0700

    fix: Clear Laravel cache on startup to fix filament-backgrounds stale cache

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 php artisan queue:work --sleep=3 --tries=3 --max-time=3600 &
 php artisan serve --host=0.0.0.0 --port=8000
